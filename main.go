5d3c3934b2926245a555cc5307a1cb26fe778bd2
1strewave <aatrutnev23@icloud.com>
2025-01-24 23:08:35 -0800

    Recoded almost full game to add Level System

--- main.go
+++ main.go
@@ -14,13 +14,64 @@ import (
 )
 
 type GameWorld struct {
-	layout    string
-	holeIndex int
+	levels        []string
+	playerPos     [2]int
+	holePositions []int
+	depth         int
 }
 
 func main() {
 	clearScreen()
 	worldBuild()
+}
+
+func worldBuild() {
+	depth := flag.Int("depth", 4, "Number of levels in the game")
+	worldSize := flag.Int("world-size", 16, "Size of each level")
+	flag.Parse()
+
+	if *depth <= 1 {
+		fmt.Println("ERR: Game depth must be greater than 1.")
+		os.Exit(1)
+	}
+	if *worldSize <= 1 {
+		fmt.Println("ERR: World size must be greater than 1.")
+		os.Exit(1)
+	}
+
+	rand.Seed(time.Now().UnixNano())
+
+	world := createGameWorld(*depth, *worldSize)
+	runGame(world)
+}
+
+func createGameWorld(depth, worldSize int) GameWorld {
+	levels := make([]string, depth)
+	holePositions := make([]int, depth)
+
+	for level := 0; level < depth; level++ {
+		levelRunes := make([]rune, worldSize)
+		for i := range levelRunes {
+			levelRunes[i] = '_'
+		}
+
+		if level == 0 {
+			levelRunes[0] = 'A'
+		}
+
+		holeIndex := rand.Intn(worldSize-2) + 1
+		holePositions[level] = holeIndex
+		levelRunes[holeIndex] = ' '
+
+		levels[level] = string(levelRunes)
+	}
+
+	return GameWorld{
+		levels:        levels,
+		playerPos:     [2]int{0, 0},
+		holePositions: holePositions,
+		depth:         depth,
+	}
 }
 
 func runGame(world GameWorld) {
@@ -33,92 +84,99 @@ func runGame(world GameWorld) {
 
 	for {
 		clearScreen()
+
 		fmt.Println("\n\033[33;1mGame Starts\033[0m")
 		fmt.Println("Use ← → to move, ↓ to jump in hole, ESC to exit")
-		fmt.Printf("\n  %s\n", world.layout)
+
+		for i, level := range world.levels {
+			if i == world.playerPos[0] {
+				fmt.Printf("%s\n", level)
+			} else {
+				fmt.Printf("%s\n", level)
+			}
+		}
 
 		if winCheck(world) {
-			fmt.Println("\n\033[32mYou won!\033[0m")
+			fmt.Println("\n\033[32mCongratulations! You won the game!\033[0m")
 			return
 		}
 
-		world.layout = keyPress(world)
+		world = keyPress(world)
+
 		time.Sleep(100 * time.Millisecond)
 	}
 }
 
-func worldBuild() {
-	worldSize := flag.Int("world-size", 16, "Specify the size of the world")
-	flag.Parse()
-
-	if *worldSize <= 1 {
-		fmt.Println("ERR: World size must be greater than 1.")
-		os.Exit(1)
-	}
-
-	rand.Seed(time.Now().UnixNano())
-
-	holeIndex := rand.Intn(*worldSize-2) + 1
-
-	worldRunes := make([]rune, *worldSize)
-	for i := range worldRunes {
-		if i == 0 {
-			worldRunes[i] = 'A'
-		} else if i == holeIndex {
-			worldRunes[i] = ' '
-		} else {
-			worldRunes[i] = '_'
-		}
-	}
-
-	world := GameWorld{
-		layout:    string(worldRunes),
-		holeIndex: holeIndex,
-	}
-
-	runGame(world)
-}
-
 func winCheck(world GameWorld) bool {
-	playerIndex := strings.Index(world.layout, "A")
-	return playerIndex == world.holeIndex
+	currentLevel := world.playerPos[0]
+	playerIndex := strings.Index(world.levels[currentLevel], "A")
+
+	return currentLevel == len(world.levels)-1 &&
+		(playerIndex == world.holePositions[currentLevel]-1 ||
+			playerIndex == world.holePositions[currentLevel]+1)
 }
 
-func keyPress(world GameWorld) string {
+func keyPress(world GameWorld) GameWorld {
 	_, key, err := keyboard.GetKey()
 	if err != nil {
 		panic(err)
 	}
 
-	index := strings.Index(world.layout, "A")
+	currentLevel := world.playerPos[0]
+	levels := make([]string, len(world.levels))
+	copy(levels, world.levels)
+
+	index := strings.Index(levels[currentLevel], "A")
 	if index == -1 {
 		panic("Player not found")
 	}
 
-	worldRunes := []rune(world.layout)
+	levelRunes := []rune(levels[currentLevel])
 
 	switch key {
 	case keyboard.KeyArrowLeft:
-		if index > 0 && worldRunes[index-1] != ' ' {
-			worldRunes[index] = '_'
-			worldRunes[index-1] = 'A'
+		if index > 0 && levelRunes[index-1] != ' ' {
+			levelRunes[index] = '_'
+			levelRunes[index-1] = 'A'
+			world.playerPos[1] = index - 1
 		}
 	case keyboard.KeyArrowRight:
-		if index < len(worldRunes)-1 && worldRunes[index+1] != ' ' {
-			worldRunes[index] = '_'
-			worldRunes[index+1] = 'A'
+		if index < len(levelRunes)-1 && levelRunes[index+1] != ' ' {
+			levelRunes[index] = '_'
+			levelRunes[index+1] = 'A'
+			world.playerPos[1] = index + 1
 		}
 	case keyboard.KeyArrowDown:
-		if index == world.holeIndex-1 || index == world.holeIndex+1 {
-			worldRunes[index] = '_'
-			worldRunes[world.holeIndex] = 'A'
+		holePos := world.holePositions[currentLevel]
+		if currentLevel < len(world.levels)-1 {
+			if index == holePos-1 || index == holePos+1 {
+				levelRunes[index] = '_'
+				levels[currentLevel] = string(levelRunes)
+
+				nextLevel := currentLevel + 1
+				nextLevelRunes := []rune(levels[nextLevel])
+
+				nextLevelRunes[holePos] = 'A'
+				world.playerPos = [2]int{nextLevel, holePos}
+
+				levels[nextLevel] = string(nextLevelRunes)
+				world.levels = levels
+			}
+		} else {
+			if index == holePos-1 || index == holePos+1 {
+				fmt.Println("\n\033[32mCongratulations! You won the game!\033[0m")
+				os.Exit(0)
+			}
 		}
 	case keyboard.KeyEsc:
 		fmt.Println("\n\033[31mGame Over\033[0m")
 		os.Exit(0)
 	}
 
-	return string(worldRunes)
+	levels[currentLevel] = string(levelRunes)
+	world.levels = levels
+
+	return world
 }
 
 func clearScreen() {
